df95163a191d31a48ee955134993038feb922315
Josh Kassman <jkassman2@gmail.com>
2017-09-22 00:18:28 -0400

    several fixes
    rename count to count.txt to try and fix read errors
    remove unnecessary imports
    add help keyword

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 var fs = require('fs');
 
 var count;
+var path = './count.txt';
 
 console.log(botID);
 
-fs.readFile('./count', function(err, data) {
+fs.readFile(path, 'utf8', function(err, data) {
   if(err) {
     return console.log(err);
   }
@@ -16,11 +16,11 @@ fs.readFile('./count', function(err, data) {
 });
 
 function writeCount(count) {
-  fs.writeFile('./count', count, function(err) {
+  fs.writeFile(path, 'utf8', count, function(err) {
     if(err) return console.log(err);
     console.log(count + " > count");
   });
-  fs.readFile('./count', function(err, data) {
+  fs.readFile(path, 'utf8', function(err, data) {
     if (err) {
       return console.log(err);
     }
@@ -31,7 +31,8 @@ function writeCount(count) {
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex1 = /LMAO/ig,
-      botRegex2 = /^\/trish  lmao count$/igm,
+      botRegex2 = /^\/trish lmao count$/igm,
+      botRegex3 = /^\/help$/igm,
       trishUserID = 49924159;
 
   //console.log(request);
@@ -47,6 +48,11 @@ function respond() {
     postMessage();
     this.res.end();
   }
+  else if(request.text && botRegex3.test(request.text)) {
+    this.res.writeHead(200);
+    postHelpMessage();
+    this.res.end();
+  }
   else {
     console.log("don't care \"" + request.text + "\"");
     this.res.writeHead(200);
@@ -58,6 +64,41 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = "The Trish LMAO count is now at " + count;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postHelpMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "This bot counts the number of times Trish says lmao. Send \"/trish lmao count\" to see the count";
 
   options = {
     hostname: 'api.groupme.com',
